610decb642275796770d3002a844f59de2d2561b
asanteb <asante@geekenforcer.com>
2021-12-12 18:05:46 -0500

    complete day1

--- src/day1.rs
+++ src/day1.rs
@@ -2,7 +2,26 @@ mod line_reader;
 
 fn main() {
     let lines = line_reader::vec_from_files("src/day1-input.txt");
-    for line in lines {
-        println!("{:?}", line);
+    let mut last_number = lines[0].parse::<i32>().unwrap();
+    let mut increase_amount = 0;
+
+    for (idx, line) in lines.iter().enumerate() {
+        let num = line.parse::<i32>().unwrap();
+
+        if idx == 0 {
+            last_number = num;
+        } else {
+            match num {
+                x if x  > last_number => {
+                    increase_amount += 1;
+                    last_number = num;
+                }
+                _ => {
+                    last_number = num;
+                }
+            }
+        }
+
     }
+    println!("Number of increases {:?}", increase_amount);
 }
